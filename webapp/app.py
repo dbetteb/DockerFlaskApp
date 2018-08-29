4ae54bb277875ef8ab4f340f07a50cee5617fdd9
dbettebeki <dimitri.bettebghor@ekimetrics.com>
2018-08-29 17:29:47 +0200

    app

--- webapp/app.py
+++ webapp/app.py
@@ -1,1 +1,13 @@
 # app.py simple api written in Flask 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/users/<string:username>')
+def hello_ekimetrics(username=None):
+    return "Welcome {} to Ekimetrics !".format(username)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=8000)
+
